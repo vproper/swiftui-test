0fbbcfdeb6474b5d214bbe5df2bc3e101cfe4be2
vproper <bengboxer@gmail.com>
2021-03-10 17:07:40 +0300

    Initial Commit

---
+++ swiftui-test/swiftui_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_testApp.swift
+//  swiftui-test
+//
+//  Created by vantuzproper on 10.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
